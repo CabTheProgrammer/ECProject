eebaaafab65ff954fb5fcd5be5b008e412af8e6f
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2021-10-26 20:44:55 -0500

    Added Remove Item admin functionality

---
+++ ECProject/RemoveProduct.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="RemoveProduct.aspx.cs" Inherits="ECProject.RemoveProduct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class ="row">
+        <h2> Remove Product</h2>
+        <asp:Label ID="Label1" runat="server" Text="Please enter the product id of the product to be deleted"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" style="width:25%;"></asp:TextBox><br><br>
+        <asp:Button ID="Button1" runat="server" Text="Delete" class ="btn btn-danger" OnClick="Button1_Click" />
+        <asp:Label ID="Label2" runat="server" ForeColor="Red"></asp:Label>
+    </div>
+</asp:Content>
---
+++ ECProject/RemoveProduct.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace ECProject
+{
+    public partial class RemoveProduct : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int itemid = Convert.ToInt32(TextBox1.Text);
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProductsConnectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("DELETE FROM PRODUCT_INFO WHERE ID=" + itemid,conn);
+
+            conn.Open();
+            if(cmd.ExecuteNonQuery()!=0)
+            {
+                Label2.Text = "Item has been successfully removed";
+            }
+            conn.Close();
+
+        }
+    }
+}
